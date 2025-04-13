1632fc858d5e0d33c3da7853981fff7036eb0b82
shreyanssatpute <136170368+shreyanssatpute@users.noreply.github.com>
2025-04-13 19:38:59 +0530

    Update SystemArchitecture.tsx

--- src/components/SystemArchitecture.tsx
+++ src/components/SystemArchitecture.tsx
@@ -7,7 +7,7 @@ const SystemArchitecture = () => {
       <h3 className="text-xl font-semibold text-center mb-6">System Architecture</h3>
       <div className="flex justify-center">
       <img 
-  src="public/lovable-uploads/hh.png" 
+  src="/lovable-uploads/hh.png" 
   alt="System Architecture Diagram" 
   className="w-[1200px] h-[600px] object-contain rounded-2xl"
 />
